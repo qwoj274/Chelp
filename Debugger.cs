53048192c4e446abee908aec403f4712276a23b6
qwoj274 <budakovnikita2006@gmail.com>
2025-11-03 13:03:56 +0500

    created and tested ini files parser

---
+++ Debugger.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace Chelp
+{
+    enum DebugTypes
+    {
+        DEBUG,
+        WARNING,
+        ERROR,
+    }
+
+    static class Debug
+    {
+        const string LOGPATH = "./debug";
+        const string LOGFILENAME = "log.txt";
+        const string LOGFILENPATH = $"{LOGPATH}/{LOGFILENAME}"; 
+        static readonly string LOGFILENAMENOEXTENSION = LOGFILENAME.Split(".").Last();
+        static FileStream _debugFile;
+        static Debug()
+        {
+            Directory.CreateDirectory(LOGPATH);
+
+            FileInfo fileInfo = new(LOGFILENPATH);
+            bool isFileExistsAndEmpty = fileInfo.Exists && Convert.ToBoolean(fileInfo.Length);
+            FileMode fileMode = isFileExistsAndEmpty ? FileMode.Append : FileMode.OpenOrCreate;
+
+            _debugFile = new(LOGFILENPATH, fileMode, FileAccess.Write);
+            if (isFileExistsAndEmpty) {
+                RawLog("\n");
+            }
+        }
+
+        static readonly Dictionary<DebugTypes, string> stringLogTypes = new()
+        {
+            { DebugTypes.DEBUG, "DBG" },
+            { DebugTypes.WARNING, "WRN" },
+            {DebugTypes.ERROR, "ERR" },
+        };
+        
+        public static string GetCurrentTimeFormat()
+        {
+            DateTime currentTime = DateTime.Now;
+            string formattedTime = currentTime.ToString(@"[dd\/MM\/yyyy HH:mm:ss.ff]");
+            return formattedTime;
+        }
+
+        public static void Log(string value, DebugTypes debugType)
+        {
+            byte[] content = Encoding.UTF8.GetBytes($"{GetCurrentTimeFormat()} [{stringLogTypes[debugType]}]: {value}\n");
+            _debugFile.Write(content, 0, content.Length);
+            _debugFile.Flush();
+        }
+        
+        public static void RawLog(string value)
+        {
+            _debugFile.Write(Encoding.UTF8.GetBytes(value, 0, value.Length));
+            _debugFile.Flush();
+        }
+    }
+}
