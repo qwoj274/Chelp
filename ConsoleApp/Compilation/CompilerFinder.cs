1789e66ee539029c3df13919febb91f95f4212e0
qwoj274 <budakovnikita2006@gmail.com>
2025-11-05 19:34:05 +0500

    added wpf

--- ConsoleApp/Compilation/CompilerFinder.cs
+++ ConsoleApp/Compilation/CompilerFinder.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
-using System.Text.Json;
 using Debugger;
 using Newtonsoft.Json;
 
---
+++ WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
